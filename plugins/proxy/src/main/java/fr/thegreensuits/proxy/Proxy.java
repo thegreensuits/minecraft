91349a30bb83db47a65ccaf26208a0bfd1460022
jijon <work@jeremybdn.fr>
2025-06-08 02:28:25 +0200

    ✨ refactor: replace RedisHelper with RedisExecutor for improved Redis connection management; enhance server initialization and shutdown logging

--- plugins/proxy/src/main/java/fr/thegreensuits/proxy/Proxy.java
+++ plugins/proxy/src/main/java/fr/thegreensuits/proxy/Proxy.java
@@ -14,7 +14,7 @@ import fr.thegreensuits.api.redis.pubsub.Channels;
 import fr.thegreensuits.api.server.manager.ServerManager;
 import fr.thegreensuits.api.server.status.ServerStatus;
 import fr.thegreensuits.api.utils.StaticInstance;
-import fr.thegreensuits.api.utils.helper.RedisHelper;
+import fr.thegreensuits.api.utils.helper.RedisExecutor;
 import fr.thegreensuits.proxy.config.ConfigManager;
 import fr.thegreensuits.proxy.listener.server.InitialServerListener;
 import fr.thegreensuits.proxy.redis.pubsub.listener.ServerCreatedListener;
@@ -29,125 +29,125 @@ import org.slf4j.LoggerFactory;
 
 @Plugin(id = "proxy", name = "Proxy", version = BuildConstants.VERSION, description = BuildConstants.DESCRIPTION)
 public class Proxy extends StaticInstance<Proxy> {
-    // - The Green Suits
-    private final TheGreenSuits thegreensuits;
-    private final ServerManager serverManager;
-    private final RedisHelper redisHelper;
+  // - The Green Suits
+  private final TheGreenSuits thegreensuits;
+  private final ServerManager serverManager;
+  private final RedisExecutor redisExecutor;
 
-    // - Velocity
-    @Getter
-    private final ProxyServer proxy;
-    private final EventManager eventManager;
+  // - Velocity
+  @Getter
+  private final ProxyServer proxy;
+  private final EventManager eventManager;
 
-    // - Configuration
-    @Getter
-    private final ConfigManager configManager;
+  // - Configuration
+  @Getter
+  private final ConfigManager configManager;
 
-    // - Misc
-    @Getter
-    private final Logger logger;
+  // - Misc
+  @Getter
+  private final Logger logger;
 
-    @Inject
-    public Proxy(ProxyServer proxy, Logger logger, @DataDirectory Path dataDirectory) {
-        super();
+  @Inject
+  public Proxy(ProxyServer proxy, Logger logger, @DataDirectory Path dataDirectory) {
+    super();
 
-        this.configManager = new ConfigManager(dataDirectory);
-        this.configManager.loadConfig();
+    this.configManager = new ConfigManager(dataDirectory);
+    this.configManager.loadConfig();
 
-        // - Initialize TheGreenSuits
-        String serverId = this.configManager.getServerId();
-        if (serverId.equals("-1")) {
-            throw new IllegalStateException("server-id is not defined in the configuration file");
-        }
-
-        this.thegreensuits = new TheGreenSuitsImpl(serverId);
-        this.serverManager = this.thegreensuits.getServerManager();
-        this.redisHelper = new RedisHelper(this.thegreensuits.getJedisPool());
-
-        this.proxy = proxy;
-        this.eventManager = proxy.getEventManager();
-        this.logger = logger;
-
-        this.serverManager.updateServer(serverId, ServerStatus.STARTING);
+    // - Initialize TheGreenSuits
+    String serverId = this.configManager.getServerId();
+    if (serverId.equals("-1")) {
+      throw new IllegalStateException("server-id is not defined in the configuration file");
     }
 
-    @Subscribe
-    public void onProxyInitialization(ProxyInitializeEvent event) {
-        this.logger.info("Proxy initializing...");
+    this.thegreensuits = new TheGreenSuitsImpl(serverId);
+    this.serverManager = this.thegreensuits.getServerManager();
+    this.redisExecutor = new RedisExecutor(this.thegreensuits.getJedisPool());
 
-        // - Register listeners
-        this.registerListeners();
+    this.proxy = proxy;
+    this.eventManager = proxy.getEventManager();
+    this.logger = logger;
 
-        // - Register current servers on proxy
-        this.registerServers();
+    this.serverManager.updateServer(serverId, ServerStatus.STARTING);
+  }
 
-        this.logger.info("Proxy initialized");
-        this.serverManager.updateServer(this.thegreensuits.getServerId(), ServerStatus.RUNNING);
-    }
+  @Subscribe
+  public void onProxyInitialization(ProxyInitializeEvent event) {
+    this.logger.info("Proxy initializing...");
 
-    private void registerListeners() {
-        this.logger.info("Registering listeners...");
+    // - Register listeners
+    this.registerListeners();
 
-        this.logger.info("Registering Redis channel listeners");
-        this.redisHelper.executeVoid(jedis -> {
-            jedis.subscribe(new ServerCreatedListener(this), Channels.SERVERS_CREATED.getChannel());
-            jedis.subscribe(new ServerUpdatedListener(this), Channels.SERVERS_UPDATED.getChannel());
-            return null;
-        });
+    // - Register current servers on proxy
+    this.registerServers();
 
-        this.logger.info("Registering Velocity events listeners");
-        this.eventManager.register(this, new InitialServerListener(this.proxy));
+    this.logger.info("Proxy initialized");
+    this.serverManager.updateServer(this.thegreensuits.getServerId(), ServerStatus.RUNNING);
+  }
 
-        this.logger.info("Listeners registered");
-    }
+  private void registerListeners() {
+    this.logger.info("Registering listeners...");
 
-    /**
-     * Register all servers on the proxy
-     */
-    private void registerServers() {
-        this.logger.info("Registering servers...");
+    this.logger.info("Registering Redis channel listeners");
+    this.redisHelper.executeVoid(jedis -> {
+      jedis.subscribe(new ServerCreatedListener(this), Channels.SERVERS_CREATED.getChannel());
+      jedis.subscribe(new ServerUpdatedListener(this), Channels.SERVERS_UPDATED.getChannel());
+      return null;
+    });
 
-        this.thegreensuits.getServerManager().getServers().forEach((id, server) -> {
-            ServerInfo serverInfo = new ServerInfo(server.getId(), server.buildInetSocketAddress());
-            this.proxy.registerServer(serverInfo);
+    this.logger.info("Registering Velocity events listeners");
+    this.eventManager.register(this, new InitialServerListener(this.proxy));
 
-            this.logger.info("Server {} registered", server.getId());
-        });
+    this.logger.info("Listeners registered");
+  }
 
-        this.logger.info("Registered {} servers", this.thegreensuits.getServerManager().getServers().size());
-    }
+  /**
+   * Register all servers on the proxy
+   */
+  private void registerServers() {
+    this.logger.info("Registering servers...");
 
-    @Subscribe
-    public void onProxyShutdown(ProxyShutdownEvent event) {
-        this.logger.info("Shutting down proxy...");
-        this.thegreensuits.close();
+    this.thegreensuits.getServerManager().getServers().forEach((id, server) -> {
+      ServerInfo serverInfo = new ServerInfo(server.getId(), server.buildInetSocketAddress());
+      this.proxy.registerServer(serverInfo);
 
-        String serverId = this.thegreensuits.getServerId();
-        this.serverManager.updateServer(serverId, ServerStatus.STOPPED);
-        this.serverManager.removeServer(serverId);
-        this.logger.info("Proxy shut down");
+      this.logger.info("Server {} registered", server.getId());
+    });
+
+    this.logger.info("Registered {} servers", this.thegreensuits.getServerManager().getServers().size());
+  }
+
+  @Subscribe
+  public void onProxyShutdown(ProxyShutdownEvent event) {
+    this.logger.info("Shutting down proxy...");
+    this.thegreensuits.close();
+
+    String serverId = this.thegreensuits.getServerId();
+    this.serverManager.updateServer(serverId, ServerStatus.STOPPED);
+    this.serverManager.removeServer(serverId);
+    this.logger.info("Proxy shut down");
+  }
+
+  @Override
+  protected int getClassId() {
+    return 1;
+  }
+
+  private class TheGreenSuitsImpl extends TheGreenSuits {
+    public TheGreenSuitsImpl(String serverId) {
+      super(serverId);
     }
 
     @Override
-    protected int getClassId() {
-        return 1;
+    public Logger getLogger() {
+      return LoggerFactory.getLogger(Proxy.class);
     }
 
-    private class TheGreenSuitsImpl extends TheGreenSuits {
-        public TheGreenSuitsImpl(String serverId) {
-            super(serverId);
-        }
+    @Override
+    public void close() {
+      Proxy.this.serverManager.updateServer(this.getServerId(), ServerStatus.STOPPING);
 
-        @Override
-        public Logger getLogger() {
-            return LoggerFactory.getLogger(Proxy.class);
-        }
-
-        @Override
-        public void close() {
-            Proxy.this.serverManager.updateServer(this.getServerId(), ServerStatus.STOPPING);
-
-            super.close();
-        }
+      super.close();
     }
+  }
 }
